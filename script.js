4c2b64a8ade39569692b7c317e6e8870cd502e4c
KarNiO <kar.ni.o@yandex.ru>
2017-10-02 15:45:45 +0500

    JS vertical output

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 $(document).on('ready', function(){
+	var my_regexp = /[a-zA-Zа-яА-ЯёЁ]/i;
 	
-	$(document).on('keydown', function(){
-
+	$(document).on('keydown', function(e){
+		var symbol = my_regexp.test(e.key)
+		var button = e.key
+		console.log(button.length);
+		console.log(symbol);
+		//console.log(e);
+		console.log(e.which);
+		if ( symbol && (button.length == 1)) {
+			$('.text_place').append('<p class="letter"' + '>' + e.key + '</p>');
+			$('.letter').fadeIn(300);
+		}
+		else {
+			return false;
+		};
 	});
 });
