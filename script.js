590bcba3f634672aba5892dfc8910c7cc3804dff
KarNiO <kar.ni.o@yandex.ru>
2017-10-02 17:53:05 +0500

    Finish

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ $(document).on('ready', function(){
 	$(document).on('keydown', function(e){
 		var symbol = my_regexp.test(e.key)
 		var button = e.key
-		console.log(button.length);
-		console.log(symbol);
-		//console.log(e);
-		console.log(e.which);
 		if ( symbol && (button.length == 1)) {
-			$('.text_place').append('<p class="letter"' + '>' + e.key + '</p>');
+			$('.text_place').append('<div class="letter"' + '>' + e.key + '</div>');
 			$('.letter').fadeIn(300);
 		}
 		else {
-			return false;
+			return true;
 		};
 	});
 });
