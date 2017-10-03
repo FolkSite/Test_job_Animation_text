0f6f07543d9b4a3c8c76f1afe28a66b32cc2d15a
KarNiO <kar.ni.o@yandex.ru>
2017-10-03 10:19:38 +0500

    Added http-server

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 $(document).on('ready', function(){
+
 	var my_regexp = /[a-zA-Zа-яА-ЯёЁ]/i;
-	
+
 	$(document).on('keydown', function(e){
-		var symbol = my_regexp.test(e.key)
-		var button = e.key
+
+		var symbol = my_regexp.test(e.key);
+		var button = e.key;
+
 		if ( symbol && (button.length == 1)) {
+		
 			$('.text_place').append('<div class="letter"' + '>' + e.key + '</div>');
-			$('.letter').fadeIn(300);
-		}
-		else {
+			$('.letter').fadeIn(3000);
 			return true;
+		
+		}
+		
+		else {
+		
+			return true;
+		
 		};
 	});
 });
